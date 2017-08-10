d1ef09eef561a9104ed3698861eadf8d3a16891b
Nick Lee <nick@tendigi.com>
2017-08-10 12:54:18 -0400

    prettified deployment script

--- lib/ezpaas/cli/commands/deployments.rb
+++ lib/ezpaas/cli/commands/deployments.rb
@@ -1,6 +1,8 @@
 require 'thor'
 require 'git'
 require 'tmpdir'
+require 'tty'
+require 'uri'
 require 'ezpaas/cli/commands/server_commands'
 require 'ezpaas/http/sse_client'
 
@@ -13,15 +15,31 @@ module EzPaaS
         option :dir, :type => :string, :default => Dir.pwd
         option :branch, :type => :string, :default => 'master'
         def push(app)
-          git = Git.open(options[:dir])
-          branch = git.branches[options[:branch]]
+          pastel = Pastel.new
+
+          dir = options[:dir]
+          branch = options[:branch]
+
+          puts 'Opening git repository at ' + pastel.blue(dir)
+          git = Git.open(dir)
+          branch = git.branches[branch]
+
           begin
             path = Dir::Tmpname.create('ezpaas') do |file|
+              puts 'Archiving ' + pastel.blue(branch) + ' branch'
               branch.archive(file, {format: 'tar'})
             end
-            sse_client.deploy(app, path) do |message|
-              puts message
+
+            url_str = URI::join(options[:server], "proxy/#{app}/").to_s
+            success_msg = pastel.green('Application deployment completed')
+            success_msg += "\n" + 'Access your application at ' + pastel.blue(url_str)
+
+            server_comm_wrap(success_msg) do
+              sse_client.deploy(app, path) do |message|
+                puts message
+              end
             end
+
           ensure
             File.delete(path)
           end
@@ -43,7 +61,20 @@ module EzPaaS
           def sse_client
             HTTP::SSEClient.new(options[:server])
           end
+
+          def server_comm_wrap(end_msg)
+            screen = TTY::Screen.new
+            msg = 'Opening connection to slug compilation container'
+            puts msg
+            puts '─' * [msg.length, screen.width].min
+            puts
+            yield
+            puts
+            puts '─' * [end_msg.length, screen.width].min
+            puts end_msg
+          end
         end
+
 
       end
     end
